34617f3d9491f7c5bf6db4fd1cd8cc847a20c8f4
Nitesh Khanduja <nitehkhanduja19943@gmail.com>
2020-05-24 13:04:13 -0800

    tic tak toe board added and winner

--- tictactoe.py
+++ tictactoe.py
@@ -1,4 +1,45 @@
 # this is game with out gui
-
 #input for loop
 # board structure
+
+
+#functions
+board =['' for x in range (10)]
+def insertLetter(letter,pos):
+    board[pos] = letter
+
+def spacIsFree(pos):
+    return board[pos] == ''
+
+
+
+def printBoard(board):
+    print ('  |   |   ')
+    print(' '+board[1]+' | '+board[2]+ '  | '+ board[3] )
+    print ('  |   |   ')
+    print('----------------')
+    print ('  |   |   ')
+    print(' '+board[4]+' | '+board[5]+ '  | '+ board[6] )
+    print ('  |   |   ')
+    print('----------------')
+    print ('  |   |   ')
+    print(' '+board[7]+' | '+board[8]+ '  | '+ board[9] )
+    print ('  |   |   ')
+    print('----------------')
+
+
+def isBoardFull():
+    if board.count(' ') >1:
+        return False
+    else: 
+        return True
+
+def isWinner(b,l):
+    return (b[1] == l and b[2] == l and b[3] == l) or
+    (b[4] == l and b[5] == l b[6]== l)or
+    (b[7] == l and b [8] == l and b [9] ==l)or
+    (b[1] == l and b[4] == l and b[7] ==l) or
+    (b[2] == l and b[5] == l and b[8] ==l) or
+    (b[3] == l and b[6] == l and b[9] ==l) or
+    (b[1] == l and b[5] == l and b[9] ==l) or
+    (b[3] == l and b[5] == l and b[7] ==l)
